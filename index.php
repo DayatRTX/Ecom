e3feb2877240060f27d671bc09304c4fdf0f07a2
DayatRTX <andhikapratama.m.enim@gmail.com>
2025-10-01 10:40:53 +0700

    01/10/25

--- index.php
+++ index.php
@@ -35,7 +35,7 @@ $result_produk = $stmt->get_result();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Toko Gitar Online</title>
+    <title>Rodaluka Acoustic</title>
     <link rel="stylesheet" href="css/style.css">
     <link rel="icon" href="assets/logo.png" type="image/png">
 </head>
